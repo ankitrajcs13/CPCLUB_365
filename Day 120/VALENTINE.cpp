62824635c7ee75e34c6363c69d3e5ed5ec6cdb4f
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2023-02-14 21:21:44 +0530

    Day 120

---
+++ Day 120/VALENTINE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int x,y; cin>>x>>y;
+	    
+        int ans = x/y;
+        
+        cout<<ans<<endl;
+	}
+	return 0;
+}
